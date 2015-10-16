1229a4ddc236370977218cfc2356186e0af749cc
Arun Gupta <arun.gupta@gmail.com>
2015-10-15 19:39:11 -0700

    escaping bucket name, fixing the N1QL query, cleaning up code

--- src/main/java/org/couchbase/sample/javaee/AirlineResource.java
+++ src/main/java/org/couchbase/sample/javaee/AirlineResource.java
@@ -2,22 +2,12 @@ package org.couchbase.sample.javaee;
 
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.CouchbaseCluster;
-import com.couchbase.client.java.document.JsonDocument;
 import com.couchbase.client.java.document.RawJsonDocument;
-import com.couchbase.client.java.document.json.JsonObject;
 import com.couchbase.client.java.query.N1qlQuery;
 import com.couchbase.client.java.query.N1qlQueryResult;
-import java.io.IOException;
-import java.io.StringReader;
-import java.util.HashSet;
-import java.util.Set;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import javax.json.Json;
-import javax.json.JsonReader;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import static com.couchbase.client.java.query.Select.select;
@@ -31,27 +21,25 @@ public class AirlineResource {
 
     CouchbaseCluster cluster;
     Bucket bucket;
-
+    
     @PostConstruct
     private void initDB() {
-        System.out.println("initDB");
-        // Connect to the Cluster
         cluster = CouchbaseCluster.create("192.168.99.100");
-
-        bucket = cluster.openBucket("travel-sample");
+        bucket = cluster.openBucket("travel-sample");;
     }
 
     @PreDestroy
     private void stopDB() {
-        // Disconnect from the cluster
         cluster.disconnect();
     }
 
     @GET
     public String getAll() {
-        N1qlQuery query = N1qlQuery.simple(select("*").from(i("travel-sample")).limit(10));
+
+        N1qlQuery query = N1qlQuery.simple(select("id").from(i(bucket.name())).limit(10));
+//        N1qlQuery query = N1qlQuery.simple(select("*").limit(10));
 //        N1qlQuery query = N1qlQuery.simple("SELECT * FROM travel-sample LIMIT 10");
-        System.out.println(query.toString());
+        System.out.println(query.statement().toString());
         N1qlQueryResult result = bucket.query(query);
         System.out.println(result.toString());
         return result.allRows().toString();
@@ -61,17 +49,5 @@ public class AirlineResource {
     @Path("{id}")
     public String getAirline(@PathParam("id") String id) {
         return bucket.get(id, RawJsonDocument.class).content();
-    }
-
-//    @POST
-//    public void addPerson(String person) throws IOException {
-//        JsonReader reader = Json.createReader(new StringReader(person));
-//        for (String key : reader.readObject().keySet()) {
-//            JsonDocument.create(key, JsonObject.fromJson(reader.readObject().getString(key)));
-//        }
-//    }
-    @DELETE
-    public void deletePerson(String key) {
-        bucket.remove(key);
     }
 }

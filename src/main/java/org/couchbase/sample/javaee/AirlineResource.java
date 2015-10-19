143bbb0d094a12fa92241098e10906a6846c5b09
Arun Gupta <arun.gupta@gmail.com>
2015-10-19 08:21:32 -0400

    getAll and getAirline are now working

--- src/main/java/org/couchbase/sample/javaee/AirlineResource.java
+++ src/main/java/org/couchbase/sample/javaee/AirlineResource.java
@@ -8,6 +8,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import static com.couchbase.client.java.query.Select.select;
 import static com.couchbase.client.java.query.dsl.Expression.i;
+import static com.couchbase.client.java.query.dsl.Expression.x;
 import javax.inject.Inject;
 
 /**
@@ -21,12 +22,11 @@ public class AirlineResource {
     @GET
     public String getAll() {
         N1qlQuery query = N1qlQuery
-                .simple(select("id")
+                .simple(select("*")
                 .from(i(database.getBucket().name()))
                 .limit(10));
-//        N1qlQuery query = N1qlQuery.simple("SELECT * FROM `travel-sample`");
 //        N1qlQuery query = N1qlQuery.simple("SELECT * FROM `travel-sample` LIMIT 10");
-//        System.out.println(query.statement().toString());
+        System.out.println(query.statement().toString());
         N1qlQueryResult result = database.getBucket().query(query);
         System.out.println(result.toString());
         return result.allRows().toString();
@@ -35,6 +35,16 @@ public class AirlineResource {
     @GET
     @Path("{id}")
     public String getAirline(@PathParam("id") String id) {
-        return database.getBucket().get(id, RawJsonDocument.class).content();
+//        N1qlQuery query = N1qlQuery
+//                .simple(select("*")
+//                .from(i(database.getBucket().name()))
+//                .where(x("id").eq(id)));
+        N1qlQuery query = N1qlQuery.simple("SELECT * from `travel-sample` WHERE id = " + id);
+        System.out.println(query.statement().toString());        
+        N1qlQueryResult result = database.getBucket().query(query);
+        if (result.finalSuccess() && !result.allRows().isEmpty()) {
+            return result.allRows().get(0).toString();
+        }
+        return null;
     }
 }
